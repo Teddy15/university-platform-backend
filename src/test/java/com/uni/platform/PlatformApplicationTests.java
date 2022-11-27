765af20809f254a1e62a8ef14e7517796a70887a
Teodora Yovcheva <t.k.yovcheva@gmail.com>
2022-11-27 20:47:35 +0200

    Update pom.xml
    Initial commit

---
+++ src/test/java/com/uni/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
