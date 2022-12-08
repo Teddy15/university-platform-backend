e6cad9c05e65ed3df9c23a3ec6d6d9bfb45f1aae
Kras13 <paskalev.1989@abv.bg>
2022-12-08 14:45:10 +0200

    AttachmentService changes.

--- src/main/java/com/uni/platform/service/AttachmentService.java
+++ src/main/java/com/uni/platform/service/AttachmentService.java
@@ -8,18 +8,17 @@ import com.uni.platform.config.AppConfig;
 import com.uni.platform.dto.attachment.AttachmentDto;
 import com.uni.platform.entity.Attachment;
 import com.uni.platform.repository.AttachmentRepository;
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.validation.constraints.NotBlank;
-import java.io.File;
+import java.io.*;
 
 @Service
 public class AttachmentService {
     private static final String UPLOAD_SUCCESS = "Successfully uploaded a file!";
+    private static final String UPLOAD_KEY = "%{key}/{filename}.{extension}";
 
     private final AttachmentRepository attachmentRepository;
     private final AppConfig appConfig;
@@ -47,15 +46,39 @@ public class AttachmentService {
     }
 
     private String uploadFileToAmazon(String fileName, String fileType, String fileContent){
-        AmazonClientConfig amazonClientConfig = new AmazonClientConfig(appConfig);
-        AmazonS3 amazonS3 = amazonClientConfig.getAmazonS3Client();
+        String key = java.util.UUID.randomUUID().toString();
+        String uploadKey = String.format(UPLOAD_KEY, key, fileName, fileType);
 
-        // todo -> get them from the appconfig
-        String bucketName = "";
-        String fileKey = "";
+        File tempFile = new File(key);
+        String uploadedFileKey = "";
 
-        byte[] contentAsBytes = fileContent.getBytes();
+        // todo -> different fileKey = tempFileName
 
-        return new String();
+        try{
+            byte[] contentAsBytes = fileContent.getBytes();
+
+            try {
+                try (FileOutputStream fileStream = new FileOutputStream(key)) {
+                    fileStream.write(contentAsBytes);
+                }
+            }
+            catch(Exception e) {
+            }
+
+            AmazonClientConfig amazonClientConfig = new AmazonClientConfig(appConfig);
+            AmazonS3 amazonS3 = amazonClientConfig.getAmazonS3Client();
+
+            PutObjectRequest request = new PutObjectRequest(
+                    appConfig.getAmazonS3Config().getBucketName(), uploadKey, tempFile);
+
+            PutObjectResult putObjectResult = amazonS3.putObject(request);
+
+            uploadedFileKey = request.getKey();
+        }
+        finally{
+            tempFile.delete();
+        }
+
+        return uploadedFileKey;
     }
 }
--- src/main/resources/db/migration/0.0.1/V0.0.1_0__create_attachment_table.sql
+++ src/main/resources/db/migration/0.0.1/V0.0.1_0__create_attachment_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS attachment(
-    id serial PRIMARY KEY,
+    file_key uuid,
     file_name VARCHAR(128) NOT NULL,
-    file_key VARCHAR(256) NOT NULL UNIQUE,
-    file_type VARCHAR(32) NOT NULL
+    file_type VARCHAR(32) NOT NULL,
+    PRIMARY KEY (file_key)
 )
