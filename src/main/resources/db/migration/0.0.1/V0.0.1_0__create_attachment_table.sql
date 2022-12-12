ec152de2a54a34d6b2a5ec1eecacaea1b5680d38
Kras13 <paskalev.1989@abv.bg>
2022-12-12 23:24:50 +0200

    Bug fix

--- src/main/resources/db/migration/0.0.1/V0.0.1_0__create_attachment_table.sql
+++ src/main/resources/db/migration/0.0.1/V0.0.1_0__create_attachment_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS attachment(
-    id serial PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     file_key TEXT NOT NULL,
     file_name VARCHAR(128) NOT NULL,
     file_type VARCHAR(32) NOT NULL
